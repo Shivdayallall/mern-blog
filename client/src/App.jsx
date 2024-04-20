c94789fc7c37ce255bf2093ce9437e0555944d55
shiv lall <shivlall0009@gmail.com>
2024-04-19 20:20:13 -0400

    intial project set up

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Dashboard } from './pages/Dashboard';
+import { Projects } from './pages/Projects';
+import { SignUp } from './pages/SignUp';
+import { SignIn } from './pages/SignIn';
+function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="/signin" element={<SignIn />} />
+				<Route path="/signup" element={<SignUp />} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default App;
